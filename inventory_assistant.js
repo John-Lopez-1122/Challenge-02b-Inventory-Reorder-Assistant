34ebcccfe0d1964d6df514afeb6f5341b6a2645b
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-01 10:50:45 -0500

    Forgot to save, Variables made

---
+++ inventory_assistant.js
@@ -0,0 +1,8 @@
+// Variables
+let itemName = "USB-C Cable"
+let unitCost = 5.50
+let currentStock = 243
+let reorderLevel = 150
+let targetStock = 400
+let weeklyDemand = 150
+let supplierLeadTimesWeeks = 2
